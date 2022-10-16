317544be8c31f81cc445d35b478dc69cb68e7685
shimeeuisuk <euisuk95@gmail.com>
2022-10-16 20:35:17 +0900

    Feat: 로그인 기능 추가

--- client/src/constants/message.js
+++ client/src/constants/message.js
@@ -3,14 +3,24 @@ const ERROR_MSG_02 = "이메일 형식을 맞춰주세요.";
 const ERROR_MSG_03 = "8자리 이상 입력해주세요.";
 const ERROR_MSG_04 = "존재하는 회원입니다.";
 const ERROR_MSG_05 = "아이디와 비밀번호를 확인해주세요.";
+const ERROR_MSG_06 = "비밀번호가 일치하지 않습니다.";
+
 const SUCCESS_MSG_01 = "회원가입에 성공했습니다!";
 const SUCCESS_MSG_02 = "로그인에 성공했습니다!";
+const SUCCESS_MSG_03 = "올바른 이메일 형식입니다.";
+const SUCCESS_MSG_04 = "올바른 비밀번호 형식입니다.";
+const SUCCESS_MSG_05 = "비밀번호가 일치합니다.";
+
 export {
   ERROR_MSG_01,
   ERROR_MSG_02,
   ERROR_MSG_03,
   ERROR_MSG_04,
   ERROR_MSG_05,
+  ERROR_MSG_06,
   SUCCESS_MSG_01,
   SUCCESS_MSG_02,
+  SUCCESS_MSG_03,
+  SUCCESS_MSG_04,
+  SUCCESS_MSG_05,
 };
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,5 +1,11 @@
+import LoginForm from "../components/login/LoginForm.jsx";
+
 const LogIn = () => {
-  return <div>LogIn</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LogIn;
